38d15e5b7109814c125cf63ef8ce139d554a79e4
rdias66 <dias.a@live.com>
2024-08-29 11:09:16 -0300

    config file for seeding and connecting to database

---
+++ config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	DBHost        string
+	DBPort        string
+	DBUser        string
+	DBPassword    string
+	DBName        string
+	AdminEmail    string
+	AdminPassword string
+}
+
+func LoadConfig() *Config {
+	return &Config{
+		DBHost:        getEnv("DB_HOST", "localhost"),
+		DBPort:        getEnv("DB_PORT", "5432"),
+		DBUser:        getEnv("DB_USER", "postgres"),
+		DBPassword:    getEnv("DB_PASSWORD", "password"),
+		DBName:        getEnv("DB_NAME", "authdb"),
+		AdminEmail:    getEnv("ADMIN_EMAIL", "admin@example.com"),
+		AdminPassword: getEnv("ADMIN_PASSWORD", "admin123"),
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	log.Printf("Warning: environment variable %s not set, using default value %s", key, defaultValue)
+	return defaultValue
+}
+
+func ConnectDatabase(config *Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
+		config.DBHost, config.DBUser, config.DBPassword, config.DBName, config.DBPort,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
