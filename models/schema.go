2dc88a2b8c0e3abf2a5fc8d9724465fd77c056dc
rdias66 <dias.a@live.com>
2024-08-30 10:44:52 -0300

    uuid added to models ids

--- models/schema.go
+++ models/schema.go
@@ -1,19 +1,32 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type User struct {
-	gorm.Model
-	Id       string `gorm:"unique;not null"`
+	Id       string `gorm:"type:uuid;primary_key;unique;not null"`
 	Email    string `gorm:"unique;not null"`
 	Password string `gorm:"not null"`
 	RoleID   string
 	Role     Role
+	gorm.Model
 }
 
 type Role struct {
-	gorm.Model
-	Id    string `gorm:"unique;not null"`
+	Id    string `gorm:"type:uuid;primary_key;unique;not null"`
 	Name  string `gorm:"unique;not null"`
 	Users []User
+	gorm.Model
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	user.Id = uuid.New().String()
+	return
+}
+
+func (role *Role) BeforeCreate(tx *gorm.DB) (err error) {
+	role.Id = uuid.New().String()
+	return
 }
